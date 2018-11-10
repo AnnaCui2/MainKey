287f83f847c35181747bee0176714e09eece22fe
Michael Ancheta <44214316+mjcancheta@users.noreply.github.com>
2018-11-09 17:37:35 -0800

    Added login and registration functionality

--- app/src/main/java/com/example/mjcan/mainkey/MainActivity.kt
+++ app/src/main/java/com/example/mjcan/mainkey/MainActivity.kt
@@ -3,29 +3,22 @@ package com.example.mjcan.mainkey
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.email_et
+import kotlinx.android.synthetic.main.activity_main.password_et
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        login_btn.setOnClickListener {
-            var statues =
-                if (username_et.text.toString().equals("Sample") && password_et.text.toString().equals("password")) "Logged in"
-                else
-                    "Login fail"
-
-
-        }
-
-        //Sends to user to home screen on button press
         val loginbtn : Button = findViewById(R.id.login_btn)
         loginbtn.setOnClickListener{
-            val intent_login = Intent(this,Home :: class.java)
-            startActivity(intent_login)
+            performLogin()
         }
 
         //Takes user to registration page
@@ -35,4 +28,36 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent_register)
         }
     }
+
+    private fun performLogin(){
+        val email : String = email_et.text.toString()
+        val password : String = password_et.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d(TAG,"Email is: " + email)
+        Log.d(TAG,"Password:: $password")
+
+        //Firebase Authentication to sign in with email and password
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).
+                addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Log.d(TAG,"Successfully signed in!")
+
+                        //Sends to user to home screen on button press
+                        val intent_login = Intent(this,Home :: class.java)
+                        startActivity(intent_login)
+                        return@addOnCompleteListener
+                    }
+                }
+                .addOnFailureListener{
+                    Log.w(TAG, "Sign in failed.")
+                    Toast.makeText(baseContext, "Invalid credentials.", Toast.LENGTH_SHORT).show()
+                }
+    }
 }
+
+private val TAG = MainActivity::class.java.getName()
--- app/src/main/java/com/example/mjcan/mainkey/RegistrationPage.kt
+++ app/src/main/java/com/example/mjcan/mainkey/RegistrationPage.kt
@@ -1,9 +1,19 @@
 package com.example.mjcan.mainkey
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_registration_page.input_email
+import kotlinx.android.synthetic.main.activity_registration_page.input_password
+import kotlinx.android.synthetic.main.activity_registration_page.btn_signup
+
 
 class RegistrationPage : AppCompatActivity() {
 
@@ -11,10 +21,42 @@ class RegistrationPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_page)
 
+        //Create account button
         val signupbtn : Button = findViewById(R.id.btn_signup)
+
         signupbtn.setOnClickListener {
-            val intent_main = Intent(this, MainActivity::class.java)
-            startActivity(intent_main)
+            performRegister()
         }
     }
+
+    private fun performRegister(){
+        val email : String = input_email.text.toString()
+        val password : String = input_password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d(TAG,"Email is: " + email)
+        Log.d(TAG,"Password:: $password")
+
+        //Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener{
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    //else if successful
+                    Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d(TAG, "Failed to create user account: ${it.message}")
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+
+        val intent_main = Intent(this, MainActivity::class.java)
+        startActivity(intent_main)
+    }
 }
+
+private val TAG = RegistrationPage::class.java.getName()
