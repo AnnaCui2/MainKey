9f35333d7712f230651c15fe29fbd8e7386e13e6
Michael Ancheta <44214316+mjcancheta@users.noreply.github.com>
2018-12-02 19:00:23 -0800

    List displays account names now

--- app/src/main/java/com/example/mjcan/mainkey/AccountInfo_Class.kt
+++ app/src/main/java/com/example/mjcan/mainkey/AccountInfo_Class.kt
@@ -1,65 +1,12 @@
 package com.example.mjcan.mainkey
 
-class acc_node {
-    var next: acc_node? = null
-    var prev: acc_node? = null
-    var data: AccountInfo? = null
-}
-
-
-class AccountInfo(accName: String, accUsername: String, accPass: String, count: Int) {
+class AccountInformation(accName: String, accUsername: String, accPass: String, count: Int) {
     /*
     Info for each account entry created by the user
      */
-
-
-    var accountName: String? = null
-    var accountUsername: String? = null
-    var accountPassword: String? = null
-    var ID: Int? = null
-
-
-    init {
-        accountName = accName
-        accountUsername = accUsername
-        accountPassword = accPass
-        ID = count
-    }
-
-    /*
-    Info to handle the linked list
-     */
-    private var head: acc_node? = null
-    var isEmpty: Boolean = head == null
-
-
-    /*
-    functions
-     */
-    fun firstNode(): acc_node? = null
-    fun lastNode(): acc_node? {
-        var node = head
-        if (node != null){
-            while (node?.next != null) {
-                node = node.next
-            }
-            return node
-        } else {
-            return null
-        }
-    }
-    fun insertBack(data: AccountInfo){
-        var newNode = acc_node()
-        newNode.data = data
-        var lastNode = this.lastNode()
-        if (lastNode != null){
-            newNode.prev = lastNode
-            lastNode.next = newNode
-        }
-        else {
-            head = newNode
-        }
-    }
+    var account_name : String? = null
+    var account_email: String? = null
+    var account_password : String? = null
 }
 
 
